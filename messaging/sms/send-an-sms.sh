0d2adcb4c3e5082bff8abccfca58f39e62e3fadf
Facek <54592241+facek-code@users.noreply.github.com>
2020-05-27 09:14:48 +0000

    Send-and-sms.sh
    Coba

--- messaging/sms/send-an-sms.sh
+++ messaging/sms/send-an-sms.sh
@@ -2,8 +2,8 @@
 source "../../config.sh"
 
 curl -X "POST" "https://rest.nexmo.com/sms/json" \
-  -d "from=$NEXMO_BRAND_NAME" \
-  -d "text=A text message sent using the Nexmo SMS API" \
-  -d "to=$TO_NUMBER" \
+  -d "from=$Desa-Sukamulya" \
+  -d "text=Testing di coba text message sent using the Nexmo SMS API" \
+  -d "to=$TO_+6289512594289" \
   -d "api_key=$NEXMO_API_KEY" \
   -d "api_secret=$NEXMO_API_SECRET"
